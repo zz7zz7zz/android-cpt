f23b11e34e146e990925fbadee954de2b62f954f
Long <longhui.yang@luzcorp.com>
2021-04-25 08:17:55 +0800

    Nothing.

--- plugin_cpt_service_register/src/main/groovy/com/module/plugin/cpt/bean/CptConfig.groovy
+++ plugin_cpt_service_register/src/main/groovy/com/module/plugin/cpt/bean/CptConfig.groovy
@@ -4,15 +4,32 @@ package com.module.plugin.cpt.bean;
 class CptConfig {
 
     String applicationName
+
+    //1.asm
+    String regiserToClass
+    String regiserToClassMethod
+
+    //2.javassist
+//    String regiserToClass
+//    String regiserToClassMethod
+    String regiserToClassMethodAdd
+    String regiserToClassMethodCode
+
+    //other:一下两个参数用于在编译过程中，需要用到引用库的绝对路径
     String arouterPath
     String fragmentPath
+
 
     @Override
     public String toString() {
         return "CptConfig{" +
                 "applicationName='" + applicationName + '\'' +
+                ", regiserToClass='" + regiserToClass + '\'' +
+                ", regiserToClassMethod='" + regiserToClassMethod + '\'' +
+                ", regiserToClassMethodAdd='" + regiserToClassMethodAdd + '\'' +
+                ", regiserToClassMethodCode='" + regiserToClassMethodCode + '\'' +
                 ", arouterPath='" + arouterPath + '\'' +
                 ", fragmentPath='" + fragmentPath + '\'' +
-                '}'
+                '}';
     }
 }
