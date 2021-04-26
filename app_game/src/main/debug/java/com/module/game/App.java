e87d07fba4ff3ee55f8c809221fc2fab53465038
Long <Long>
2021-04-26 18:28:57 +0800

    optimize

---
+++ app_game/src/main/debug/java/com/module/game/App.java
@@ -0,0 +1,41 @@
+package com.module.game;
+
+import com.module.BaseApplication;
+
+public class App extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public boolean isComponentCodeIn(String component) {
+        return false;
+    }
+
+    @Override
+    public boolean isComponentConfigured(String component) {
+        return false;
+    }
+
+    @Override
+    public String getVersionName() {
+        return null;
+    }
+
+    @Override
+    public int getVersionCode() {
+        return 0;
+    }
+
+    @Override
+    public String getChannel() {
+        return null;
+    }
+
+    @Override
+    public String getProduct() {
+        return null;
+    }
+}
