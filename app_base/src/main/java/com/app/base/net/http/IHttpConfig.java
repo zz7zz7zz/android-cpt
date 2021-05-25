4f822bf38ad6ad146d0abf00b0b0e310751b99d4
Long <Long>
2021-05-25 17:24:08 +0800

    添加友盟统计

---
+++ app_base/src/main/java/com/app/base/net/http/IHttpConfig.java
@@ -0,0 +1,26 @@
+package com.app.base.net.http;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.Interceptor;
+
+public interface IHttpConfig {
+
+    String getBaseUrl();
+
+    int getConnectTimeout();
+
+    int getReadTimeout();
+
+    int getWriteTimeout();
+
+    Interceptor[] getInterceptors();
+
+    HostnameVerifier getHostnameVerifier();
+
+    X509TrustManager getSSLTrustManager();
+
+    SSLSocketFactory getSSLSocketFactory(X509TrustManager sslTrustManager);
+}
